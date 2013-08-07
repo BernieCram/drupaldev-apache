9110e750fc0dfa8c7de361795b4158cb46c0bb2f
digital006 <michael.digital.bell@googlemail.com>
2013-08-07 19:55:35 +0100

    Ensure folder is created on first run

--- manifests/example.pp
+++ manifests/example.pp
@@ -1,28 +1,32 @@
 # Define your sites here
 $sites = [
-'mikebell',
-'d8'
+    'mikebell',
+    'd8'
 ]
 
 # Magic
 define mySites {
-apache::vhost { "${name}.drupal.dev":
-  server_name   => "${name}.drupal.dev",
-  serveraliases => [
-  ],
-  docroot       => "/var/www/${name}.drupal.dev",
-  port          => '80',
-  env_variables => [
-  ],
-  priority      => '1',
-}
+  apache::vhost { "${name}.drupal.dev":
+    server_name   => "${name}.drupal.dev",
+    serveraliases => [
+    ],
+    docroot       => "/var/www/${name}.drupal.dev",
+    port          => '80',
+    env_variables => [
+    ],
+    priority      => '1',
+  }
 
-mysql::db { $name:
-  user     => $name,
-  password => $name,
-  host     => 'localhost',
-  grant    => ['all'],
-}
+  mysql::db { $name:
+    user     => $name,
+    password => $name,
+    host     => 'localhost',
+    grant    => ['all'],
+  }
+
+  file { "/var/www/${name}.drupal.dev":
+    ensure => "directory",
+  }
 }
 
 mySites { $sites: }
