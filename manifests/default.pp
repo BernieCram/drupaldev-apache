39e8ba17bfa18b611d309ae4bc0ebe3baa072a2d
digital006 <michael.digital.bell@googlemail.com>
2013-10-30 11:50:08 +0000

    Use php 5.3.27 instead of 5.4 :(

--- manifests/default.pp
+++ manifests/default.pp
@@ -12,11 +12,7 @@ Class['::apt::update'] -> Package <|
 and title != 'software-properties-common'
 |>
 
-    apt::key { '4F4EA0AAE5267A6C': }
-
-apt::ppa { 'ppa:ondrej/php5-oldstable':
-  require => Apt::Key['4F4EA0AAE5267A6C']
-}
+apt::ppa { "ppa:rip84/php5": }
 
 package { [
     'build-essential',
